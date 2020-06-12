61405c02a3ce398aa99271d96185a5044f4c59f3
huang.jian <hjj491229492@hotmail.com>
2020-06-12 10:15:41 +0800

    test: ✅cover create command implement

---
+++ __tests__/create.spec.ts
@@ -0,0 +1,22 @@
+// internal
+import { render, RenderType } from '../lib/create';
+
+describe('create new component', () => {
+  it('should render default export', () => {
+    expect(render(RenderType.INDEX, 'Rate')).toMatchSnapshot();
+  });
+
+  it('should render page template', () => {
+    expect(render(RenderType.PAGE, 'Rate')).toMatchSnapshot();
+  });
+
+  it('should render component template with default props', () => {
+    expect(render(RenderType.COMPONENT, 'Rate')).toMatchSnapshot();
+  });
+
+  it('should render component template without default props', () => {
+    expect(
+      render(RenderType.COMPONENT, 'Rate', { withoutDefaultProps: true })
+    ).toMatchSnapshot();
+  });
+});
